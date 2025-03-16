6d15e90ac68958848b1de218ea89b50dd8899764
alfredmatthew <51317708+alfredmatthew@users.noreply.github.com>
2025-03-16 21:05:45 +0700

    first commit

---
+++ src/main/java/programmerzamannow/restful/entity/Contact.java
@@ -0,0 +1,37 @@
+package programmerzamannow.restful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    private String phone;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL)
+    private List<Address> addresses;
+}
---
+++ src/main/java/programmerzamannow/restful/repository/AddressRepository.java
@@ -0,0 +1,20 @@
+package programmerzamannow.restful.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import programmerzamannow.restful.entity.Address;
+import programmerzamannow.restful.entity.Contact;
+import programmerzamannow.restful.entity.User;
+import programmerzamannow.restful.model.AddressResponse;
+import programmerzamannow.restful.model.ContactResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
+    Optional<Address> findFirstByContactAndId(Contact contact, String id);
+
+    List<Address> findAllByContact(Contact contact);
+}
